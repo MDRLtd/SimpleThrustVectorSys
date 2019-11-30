7870c8bfd9fcaa9f52f81bfc3e657e304be5b476
MDRLtd <51048146+MDRLtd@users.noreply.github.com>
2019-11-30 16:55:32 +0100

    added features
    more complete set of assets to complete project succesfully

---
+++ Sd_card (datalog code met hoogtemeter)/Sd_card.ino
@@ -0,0 +1,288 @@
+#include <SD.h> //Load SD library
+#include <Wire.h>
+#include "i2c.h"
+#include "i2c_BMP280.h"
+#include <TimerOne.h>
+
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+  
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes); 
+  uint8_t index=0;
+  while (Wire.available())
+    Data[index++]=Wire.read();
+}
+
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
+
+
+BMP280 bmp280;
+int chipSelect = 4; //chip select pin for the MicroSD Card Adapter
+File file; // file object that is used to read and write data
+
+  long int ti;
+
+  volatile bool intFlag=false;
+
+long int cpt=0;
+
+void setup() {
+  Serial.begin(9600); // start file connection to print out debug messages and data
+  Wire.begin();
+  
+  // Set accelerometers low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS,29,0x06);
+  // Set gyroscope low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS,26,0x06);
+ 
+  
+  // Configure gyroscope range
+  I2CwriteByte(MPU9250_ADDRESS,27,GYRO_FULL_SCALE_1000_DPS);
+  // Configure accelerometers range
+  I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_4_G);
+  // Set by pass mode for the magnetometers
+  I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
+  
+  // Request continuous magnetometer measurements in 16 bits
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x16);
+  
+   pinMode(13, OUTPUT);
+  Timer1.initialize(1000);         // initialize timer1, and set a 1/2 second period
+  Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
+ 
+
+  // Store initial time
+  ti=millis();
+  pinMode(chipSelect, OUTPUT);
+  if (!SD.begin(chipSelect)) { // Initialize SD card
+    file.println("Could not initialize SD card."); // if return value is false, something went wrong.
+  }
+
+  if (SD.exists("file.txt")) { // if "file.txt" exists, fill will be deleted
+    Serial.println("File exists.");
+    if (SD.remove("file.txt") == true) {
+      Serial.println("Successfully removed file.");
+    } else {
+      Serial.println("Could not remove file.");
+    }
+  }
+
+  Serial.print("Probe BMP280: ");
+  if (bmp280.initialize()) Serial.println("Sensor found");
+  else
+  {
+   Serial.println("Sensor missing");
+    while (1) {}
+  }
+
+  // onetime-measure:
+  bmp280.setEnabled(0);
+  bmp280.triggerMeasurement();
+}
+
+void callback()
+{ 
+  intFlag=true;
+  digitalWrite(13, digitalRead(13) ^ 1);
+}
+
+void loop() {
+
+  while (!intFlag);
+  intFlag=false;
+  
+  // Display time
+  Serial.print (millis()-ti,DEC);
+  Serial.print ("\t");
+
+  
+  // _______________
+  // ::: Counter :::
+  
+  // Display data counter
+// Serial.print (cpt++,DEC);
+//  Serial.print ("\t");
+  
+ 
+ 
+  // ____________________________________
+  // :::  accelerometer and gyroscope ::: 
+
+  // Read accelerometer and gyroscope
+  uint8_t Buf[14];
+  I2Cread(MPU9250_ADDRESS,0x3B,14,Buf);
+  
+  // Create 16 bits values from 8 bits data
+  
+  // Accelerometer
+  int16_t ax=-(Buf[0]<<8 | Buf[1]);
+  int16_t ay=-(Buf[2]<<8 | Buf[3]);
+  int16_t az=Buf[4]<<8 | Buf[5];
+
+  // Gyroscope
+  int16_t gx=-(Buf[8]<<8 | Buf[9]);
+  int16_t gy=-(Buf[10]<<8 | Buf[11]);
+  int16_t gz=Buf[12]<<8 | Buf[13];
+  
+    // Display values
+  
+  // Accelerometer
+  Serial.print (ax,DEC); 
+  Serial.print ("\t");
+  Serial.print (ay,DEC);
+  Serial.print ("\t");
+  Serial.print (az,DEC);  
+  Serial.print ("\t");
+  
+  // Gyroscope
+  Serial.print (gx,DEC); 
+  Serial.print ("\t");
+  Serial.print (gy,DEC);
+  Serial.print ("\t");
+  Serial.print (gz,DEC);  
+  Serial.print ("\t");
+
+  
+  // _____________________
+  // :::  Magnetometer ::: 
+
+  
+  // Read register Status 1 and wait for the DRDY: Data Ready
+  
+  uint8_t ST1;
+  do
+  {
+    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+  }
+  while (!(ST1&0x01));
+
+  // Read magnetometer data  
+  uint8_t Mag[7];  
+  I2Cread(MAG_ADDRESS,0x03,7,Mag);
+  
+
+  // Create 16 bits values from 8 bits data
+  
+  // Magnetometer
+  int16_t mx=-(Mag[3]<<8 | Mag[2]);
+  int16_t my=-(Mag[1]<<8 | Mag[0]);
+  int16_t mz=-(Mag[5]<<8 | Mag[4]);
+  
+  
+  // Magnetometer
+  Serial.print (mx+200,DEC); 
+  Serial.print ("\t");
+  Serial.print (my-70,DEC);
+  Serial.print ("\t");
+  Serial.print (mz-700,DEC);  
+  Serial.print ("\t");
+  
+  
+  
+  // End of line
+ // Serial.println("");
+  
+  bmp280.awaitMeasurement();
+
+  int tijd;
+  tijd = millis();
+
+  float temperature;
+  bmp280.getTemperature(temperature);
+
+  float pascal;
+  bmp280.getPressure(pascal);
+
+  static float meters, metersold;
+  bmp280.getAltitude(meters);
+  metersold = (metersold * 10 + meters) / 11;
+
+  bmp280.triggerMeasurement();
+
+  Serial.print(" HeightPT1: ");
+  Serial.print(metersold);
+  Serial.print(" m; Height: ");
+  Serial.print(meters);
+  Serial.print(" m; Pressure: ");
+  Serial.print(pascal);
+  Serial.print(" Pa; T: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+  file = SD.open("file.txt", FILE_WRITE); // open "file.txt" to write data
+  if (file) {
+  file.println("Tijdstip: !");
+  file.print(tijd); // schrijf waarden naar bestand
+  file.print(" ms. ");
+  file.println("Temperatuur: @");
+  file.print(temperature);
+  file.print(" *C. ");
+  file.println("Druk: #");
+  file.print(pascal);
+  file.print(" Pa.");
+  file.println("Hoogte oud: $");
+  file.print(metersold);
+  file.print(" m. ");
+  file.println("Hoogte: %");
+  file.print(meters);
+  file.print(" m.");
+  file.println("Versnellingsmeter"); 
+  file.print("^"); 
+  file.print (ax,DEC); 
+  file.print ("\t");
+  file.print("&");
+  file.print (ay,DEC);
+  file.print ("\t");
+  file.print("*");
+  file.print (az,DEC);  
+  file.print ("\t");
+  file.println("Gyroscoop");
+  file.print("(");  
+  file.print (gx,DEC); 
+  file.print ("\t");
+  file.print(")");
+  file.print (gy,DEC);
+  file.print ("\t");
+  file.print("<");
+  file.print (gz,DEC);  
+  file.print ("\t");
+  file.println("Magnetometer"); 
+  file.print(">"); 
+  file.print (mx+200,DEC); 
+  file.print ("\t");
+  file.print("?");
+  file.print (my-70,DEC);
+  file.print ("\t");
+  file.print("+");
+  file.print (mz-700,DEC);  
+  file.print ("\t");
+  file.close(); // close file
+    Serial.print("Data naar sd geschreven"); // debug output: show written number in serial monitor
+  } else {
+    Serial.println("Could not open file (writing).");
+  }
+}
---
+++ Sorting/SORT N SHIT/Final/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+
+namespace Translate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirname = "";
+            string dir = @"E:\Sort1";
+            string data = @"E:\infromat\Sort\FINAL\realdatacopy.TXT"; //paste the url to the data here
+            string[] sign = {"!", "@", "#", "$", "%", "*", "?", "&"};
+            string[] paths = {
+                "Tijd.txt",
+                "Temperatuur.txt",
+                "Druk.txt",
+                "HoogtePT1.txt",
+                "Hoogte.txt",
+                "VersnellingALL.txt",
+                "GyroscoopALL.txt",
+                "MagnetometerALL.txt"
+            };
+
+            char[] triminbois =  {'m', 's', ';', 'C', 'P', 'a', ' ', '*'};
+
+            int i = 0;
+
+            try{
+                if(Directory.Exists(dir)){
+
+                    if(Directory.GetFiles(dir) != null){
+
+                        Console.Write("Directory at {0} isnt empty. Want to create a clear Directory? (y/anything other than y) ", dir);
+
+                        if(Console.ReadLine() == "y"){
+
+                            Console.Write("Name the new directory: ");
+                            dirname = Console.ReadLine();
+
+                            if(dirname == ""){
+
+                                Console.WriteLine("The files won't be created in a folder. Are you sure you want to proceed? (y/anything other than y) ");
+                                string a = Console.ReadLine();
+
+                                if(a == "y"){
+
+                                    Directory.CreateDirectory(@"E:\" + dirname);
+
+                                } else {
+
+                                    while(Directory.Exists(@"E:\" + dirname)){
+
+                                        Console.Write("Invalid directory. Choose a different name: ");
+                                        dirname = Console.ReadLine();
+                                    }
+
+                                    if(!Directory.Exists(@"E:\" + dirname)){
+
+                                        Directory.CreateDirectory( @"E:\" + dirname);
+                                        Console.WriteLine(@"Created new directory at E:\{0} press any key to continue", dirname);
+                                    }
+                                }
+                            } else{
+                                while(Directory.Exists(@"E:\" + dirname)){
+                                    Console.Write("Invalid directory. Choose a different name: ");
+                                    dirname = Console.ReadLine();
+                                }
+                                if(!Directory.Exists(@"E:\" + dirname)){
+                                    Directory.CreateDirectory(@"E:\" + dirname);
+                                    Console.WriteLine(@"Created new directory at E:\{0} press any key to continue", dirname);
+                                }
+                            }
+
+                        } else {
+
+                            Console.Write("start fresh? (y/anything other than y) ");
+                            string ans = Console.ReadLine();
+
+                            if(ans == "y"){
+
+                                Console.WriteLine("oki");
+
+                                foreach(string p in paths){
+                                    if(File.Exists(p)){
+                                        Console.WriteLine("\tdeleting {0}", p);
+                                        File.Delete(p);
+                                    }
+                                }
+                            } else Console.WriteLine("mkay");
+                        }
+                    }
+                } else {
+                    Directory.CreateDirectory(dir);
+                }
+            } catch (Exception e){
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("Press enter to begin the sorting process");
+
+            try{
+
+                using(StreamReader sr = File.OpenText(data)){
+
+                    string s = "";
+
+                    s = sr.ReadLine();
+
+                    while(s != null){
+
+                        for(i = 0; i < sign.Length; i++){
+
+                            if(s.Contains(sign[i])){
+
+                                int index = 0;
+                                index = s.IndexOf(sign[i]);
+                                string towriteto = @"E:\" + dirname + @"\" + paths[i];
+                                using(StreamWriter sw = File.AppendText(towriteto)){
+                                    Console.WriteLine("\t{0}", s.Substring(index + 2).Trim(triminbois));
+                                    sw.WriteLine(s.Substring(index + 2).Trim(triminbois));
+                                }   
+                            }
+                        }
+                    s = sr.ReadLine();
+                    }
+                }
+            }  
+            catch(Exception e){
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\n\n|~~~~ \t D\tO\tN\tE\t ~~~~|" );
+            Console.ReadLine();
+        } 
+    }
+}
